e7fb1ad01c99144bc94fd2e8339d84f52abe020a
abu munib <amengdev@gmail.com>
2025-04-26 23:26:58 +0800

    kmp preprocessing

--- string_match.py
+++ string_match.py
@@ -1,4 +1,6 @@
 def naive_string_matcher(target, pattern):
+    found_at = []
+    
     n = len(target)
     m = len(pattern)
 
@@ -6,8 +8,39 @@ def naive_string_matcher(target, pattern):
         # print("Current Pattern: " + pattern)
         # print("Current Target: " + target[s:s + m])
         if pattern == target[s:s + m]:
-            return True
+            found_at.append(s) 
 
-    return False
+    return found_at
 
+def kmp_preprocessing(pattern):
+    lps = [0 for _ in range(len(pattern))]
+
+    l = 0
+    for i in range(1, len(pattern)):
+        if pattern[l] == pattern[i]:
+            l += 1
+            lps[i] = l
+            i += 1
+        else:
+            if l != 0:
+                l = lps[l - 1]
+            else:
+                i += 1
+
+    return lps
+
+def knuth_morris_pratt_matcher(target, pattern):
+
+    lps = kmp_preprocessing(pattern)
+
+    j = 0
+    for i in range(len(target)):
+        if pattern[j] != target[i]:
+            if i == 0:
+                i += 1
+            else:
+                j = lps[j - 1]
+        else:
+            j += 1
+            i += 1
 
--- test_string_match.py
+++ test_string_match.py
@@ -9,12 +9,17 @@ class TestStringMatchingAlgorithm(unittest.TestCase):
             {
                 "input": "xxcakedxxxxxcakesxxx",
                 "pattern": "cakes",
-                "expected": True
+                "expected": [12]
             },
             {
                 "input": "abcde",
                 "pattern": "cde",
-                "expected": True
+                "expected": [2]
+            },
+            {
+                "input": "abcabcabcabc",
+                "pattern": "abcabc",
+                "expected": [0, 3, 6]
             }
         ]
 
@@ -24,4 +29,11 @@ class TestStringMatchingAlgorithm(unittest.TestCase):
                 result = string_match.naive_string_matcher(case["input"], case["pattern"])
                 self.assertEqual(result, case["expected"])
 
+
+    def test_kmp_preprocessing(self):
+        input1 = "ababcabab"
+        lps_expected1 = [0, 0, 1, 2, 0, 1, 2, 3, 4]
+        lps_actual1 = string_match.kmp_preprocessing(input1)
+
+        self.assertEqual(lps_actual1, lps_expected1)
 
